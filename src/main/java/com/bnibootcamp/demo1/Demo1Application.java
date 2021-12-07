e75278429ac2a35cf446dacebf7738e476044aa5
latifis <latifirfansyah@gmail.com>
2021-12-07 19:46:29 +0700

    initial commit

---
+++ src/main/java/com/bnibootcamp/demo1/Demo1Application.java
@@ -0,0 +1,15 @@
+package com.bnibootcamp.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+}
